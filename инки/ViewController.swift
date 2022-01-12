e1711195ae0adf4208722d0480ca287bc806652e
Adilet <bajzakov03@bk.ru>
2022-01-12 20:16:09 +0600

    Initial Commit

---
+++ инки/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  инки
+//
+//  Created by Adilet Baizakov on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+// UIView or View - компонент (холст) является самым базовым UI компонентом
+
+
+// Constrain - некий параметр который отвечает за позицию View элемента на экране
+
+// ledding trading top bottom - имеет отступы
+
